42455dee703fadaf575574fff64081e7084299e4
Arondilbe <pinodominique@gmail.com>
2024-03-09 16:06:12 +0100

    refactor(helpers): split helpers file

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,5 @@
 export * as commandLine from './commandLine';
 export * as packageCreation from './packageCreation';
 export * as configFile from './configFile';
+export * as generatePackage from './generatePackage';
+export * as folder from './folder';
