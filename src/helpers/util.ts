3e2307468f80a1b207cfd95e3b9469274a229311
Arondilbe <pinodominique@gmail.com>
2024-03-18 13:20:39 +0100

    refactor(util): user string parser from external package

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,53 +1,8 @@
+import stringParametersParser from '@arondilbe/string-parameters-parser';
 import chalk from 'chalk';
 
 import { util as utilConfigurations } from '../configurations';
 import { PackageGenerationErrorType } from '../types';
-
-/**
- * Return a message with its parameters set to their corresponding value
- * @param {string} message The message without its parameter values
- * @param {Record<string, string>} parameters The list of message's parameters and their corresponding values
- * @returns {string} The message with its parameters set to their corresponding value
- * @throws {Error} When at least one parameter passed in the parameters object is not found in the message
- */
-export const getMessageWithParameterValues = (
-  message: string,
-  parameters: Record<string, string>,
-): string => {
-  const {
-    STRING_PARAMETER_SYMBOL: stringParameterSymbol,
-    PACKAGE_GENERATION_ERROR: packageGenerationError,
-  } = utilConfigurations;
-  const parametersKeys = Object.keys(parameters);
-  let messageWithParameterValues = message;
-  const notFoundParameters: string[] = [];
-  parametersKeys.forEach((parameterKey) => {
-    const parameterInMessage = `${stringParameterSymbol}${parameterKey}`;
-    if (!message.includes(parameterInMessage)) {
-      notFoundParameters.push(parameterInMessage);
-    }
-    messageWithParameterValues = messageWithParameterValues.replace(
-      parameterInMessage,
-      parameters[parameterKey],
-    );
-  });
-  if (notFoundParameters.length) {
-    let notFoundParametersMessage: string = '';
-    notFoundParameters.forEach(
-      (parameter) =>
-        (notFoundParametersMessage = notFoundParametersMessage.concat(
-          `\n- ${parameter.slice(1)}`,
-        )),
-    );
-
-    throw Error(
-      chalk.red(
-        `${packageGenerationError['Parameter not found in message']} ${notFoundParametersMessage}`,
-      ),
-    );
-  }
-  return messageWithParameterValues;
-};
 
 /**
  * Throw an error with parameters set to their corresponding values and with the right color format
@@ -60,9 +15,11 @@ export const throwPackageGenerationError = (
   parameters: Record<string, string>,
 ) => {
   const message = utilConfigurations.PACKAGE_GENERATION_ERROR[errorType];
-  const messageWithParameterValues = getMessageWithParameterValues(
-    message,
-    parameters,
-  );
+  const messageWithParameterValues =
+    stringParametersParser.getStringWithParameterValues(
+      utilConfigurations.STRING_PARAMETER_SYMBOL,
+      message,
+      parameters,
+    );
   throw Error(chalk.red(`${errorType}: ${messageWithParameterValues}`));
 };
