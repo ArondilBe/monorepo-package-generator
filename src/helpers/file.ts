3909f470edb1e2124716839330a3b0c508abf133
Arondilbe <pinodominique@gmail.com>
2024-04-02 08:37:55 +0200

    feat(file): add copy of common files
    Package generation now copy all common files and ignore package type folders
    
    #46

--- src/helpers/file.ts
+++ src/helpers/file.ts
@@ -1,10 +1,45 @@
-import { existsSync } from 'fs';
-import { resolve } from 'path';
+import { cpSync, existsSync } from 'fs';
+import { join, resolve } from 'path';
 
 /**
- *
+ * Check if a file or a folder exists
  * @param {string} filePath The path of the file
  * @returns {boolean} If the file exists or not
  */
 export const doesFileOrFolderExist = (filePath: string): boolean =>
   existsSync(resolve(filePath));
+
+/**
+ * Copy all the commonFiles
+ * @param {string} destinationPath The folder where the common files should be copied
+ * @param {string} sourcePath The folder from which the common files should be copied
+ * @param {string[]} packageTypeFolders An array of package type folders name to be ignored during the copy
+ */
+export const copyCommonFiles = (
+  destinationPath: string,
+  sourcePath: string,
+  packageTypeFolders?: string[],
+): void => {
+  cpSync(sourcePath, destinationPath, {
+    recursive: true,
+    filter:
+      packageTypeFolders && packageTypeFolders.length
+        ? (folder: string) => {
+            for (
+              let typeIndex = 0;
+              typeIndex < packageTypeFolders.length;
+              typeIndex += 1
+            ) {
+              if (
+                // Adding the source path to be sure to only skip the package types folder an not some
+                // folders/files with the same name in other folders
+                folder.includes(join(sourcePath, packageTypeFolders[typeIndex]))
+              ) {
+                return false;
+              }
+            }
+            return true;
+          }
+        : undefined,
+  });
+};
